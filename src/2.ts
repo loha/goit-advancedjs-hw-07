aef821f1a4d927fdf94cb7adac60559767287724
Oleksii Nosov <o.nosov@turbogames.io>
2025-01-16 18:16:48 +0200

    MAIN

--- src/2.ts
+++ src/2.ts
@@ -15,16 +15,7 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(public name: string, private department: string, protected salary: number) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -32,8 +23,16 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+    this.salary += 10000;
+  }
 }
 
+const employee: Employee = new Employee('Mark', 'IT', 48000);
+console.log(employee.getEmployeeDetails());
+
+const manager: Manager = new Manager('John', 'IT', 74000);
+console.log(manager.getEmployeeDetails());
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    private tenants: Array<Person> = [];
+
+    constructor(protected key: Key) {}
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    public openDoor(key: Key): void {
+        const houseKeySignature = this.key.getSignature();
+        const personKeySignature = key.getSignature();
+        this.door = houseKeySignature === personKeySignature;
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +49,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
